a0c4682f287bf9f1dd6f6882cb4c6214c8fc5f6a
muhammedrashidm <rashidotz@gmail.com>
2024-06-30 18:28:35 +0530

    -cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors: true});
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: [/^http:\/\/localhost:\d+$/],// Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials like cookies
+  };
+  app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
