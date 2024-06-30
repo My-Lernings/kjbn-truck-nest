56626de9e03cc36d3d6a67b57e86f5080c0fa382
muhammedrashidm <rashidotz@gmail.com>
2024-06-30 10:12:21 +0530

    -port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
 // docker run --rm -p 3000:3000 nestjs-app
