d5aa7a47e80cf1b4f388954a140ace38c0dd414d
muhammedrashidm <rashidotz@gmail.com>
2024-06-30 22:07:47 +0530

    -cors new 1

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   const corsOptions: CorsOptions = {
-    origin: [/^http:\/\/localhost:\d+$/,'https://66817564cd6edb088128686e--chic-froyo-63e35e.netlify.app/'],// Replace with your frontend URL
+    origin: [/^http:\/\/localhost:\d+$/, 'https://66817564cd6edb088128686e--chic-froyo-63e35e.netlify.app/'],// Replace with your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials like cookies
   };
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  })
   app.enableCors(corsOptions);
   await app.listen(process.env.PORT || 4000);
 }
